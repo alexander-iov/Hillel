eb72a17597f2cabd82a7e93a5001043b63f31771
Alexander <sasha.iovsa@gmail.com>
2020-12-08 21:32:17 +0200

    Add constructor

--- src/lesson4/task1/Car.java
+++ src/lesson4/task1/Car.java
@@ -3,4 +3,11 @@ package lesson4.task1;
 public class Car {
     private int weight;
     private int price;
+
+    public Car(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+
 }
