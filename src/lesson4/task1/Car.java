44d4556f41261d13a7d20257e0d73ec643de1a6d
Alexander <sasha.iovsa@gmail.com>
2020-12-08 22:02:52 +0200

    Implement interface Movable

--- src/lesson4/task1/Car.java
+++ src/lesson4/task1/Car.java
@@ -1,6 +1,26 @@
 package lesson4.task1;
 
-public class Car {
+public class Car implements Movable{
     private int weight;
     private int price;
+
+    @Override
+    public void move() {
+        System.out.println("Еду");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Стою");
+    }
+
+    @Override
+    public void moveLeft() {
+        System.out.println("Поворачиваю налево");
+    }
+
+    @Override
+    public void moveRight() {
+        System.out.println("Поворачиваю направо");
+    }
 }
