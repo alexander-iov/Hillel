86aae967b2040790b0a6eb7a6000737f9a2b0ffb
Alexander <sasha.iovsa@gmail.com>
2020-12-08 22:43:23 +0200

    Init commit

---
+++ src/lesson4/task3/ArraySorting.java
@@ -0,0 +1,17 @@
+package lesson4.task3;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        int[] array = {0, 2, 4 , -10, -16, 8, 7, 3 , 1, 11};
+        calcArrayPow(sorting(array));
+    }
+
+    public static void calcArrayPow(int[] array){
+
+    }
+
+    public static int[] sorting(int[] array){
+        int[] sortArray = new int[array.length];
+        return sortArray;
+    }
+}
