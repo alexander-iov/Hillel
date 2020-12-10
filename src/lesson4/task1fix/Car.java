fa005468f8f2dba02bb9bbc2677a8086d31f0849
Alexander <sasha.iovsa@gmail.com>
2020-12-10 17:54:47 +0200

    create constr

--- src/lesson4/task1fix/Car.java
+++ src/lesson4/task1fix/Car.java
@@ -3,4 +3,9 @@ package lesson4.task1fix;
 public class Car {
     private int weight;
     private int price;
+
+    public Car(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
 }
