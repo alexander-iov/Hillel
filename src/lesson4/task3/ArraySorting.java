2df5c8c68e3846d2387635f71ae0851f4cf6a36e
Alexander <sasha.iovsa@gmail.com>
2020-12-10 10:53:27 +0200

    Add sorting and pow methods

--- src/lesson4/task3/ArraySorting.java
+++ src/lesson4/task3/ArraySorting.java
@@ -1,17 +1,36 @@
 package lesson4.task3;
 
+import java.util.Arrays;
+
 public class ArraySorting {
     public static void main(String[] args) {
-        int[] array = {0, 2, 4 , -10, -16, 8, 7, 3 , 1, 11};
+        int[] array = {0, 2, 4, -10, -16, 8, 7, 3, 1, 11};
         calcArrayPow(sorting(array));
     }
 
-    public static void calcArrayPow(int[] array){
-
+    public static void calcArrayPow(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= array[i];
+        }
+        sorting(array);
+        System.out.print(Arrays.toString(array));
     }
 
-    public static int[] sorting(int[] array){
-        int[] sortArray = new int[array.length];
-        return sortArray;
+    private static int[] sorting(int[] array) {
+
+        int n = array.length;
+        int temp;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        return array;
     }
 }
