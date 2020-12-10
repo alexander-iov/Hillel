78b21aad6114b2413efbf8805173fbbdcca4a834
Alexander <sasha.iovsa@gmail.com>
2020-12-10 17:58:40 +0200

    implement interface

--- src/lesson4/task1fix/Car.java
+++ src/lesson4/task1fix/Car.java
@@ -1,6 +1,26 @@
 package lesson4.task1fix;
 
-public class Car {
+public class Car implements Movable{
     private int weight;
     private int price;
+
+    @Override
+    public void move() {
+        System.out.println("Move");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stop");
+    }
+
+    @Override
+    public void turnRight() {
+        System.out.println("Turn Right");
+    }
+
+    @Override
+    public void turnLeft() {
+        System.out.println("Turn Left");
+    }
 }
