f844110a035b7f6808486b7022f0e8a449f12404
Alexander <sasha.iovsa@gmail.com>
2020-12-12 18:38:56 +0200

    Create first method

--- src/lesson5/task2/StringWork.java
+++ src/lesson5/task2/StringWork.java
@@ -1,4 +1,19 @@
 package lesson5.task2;
 
 public class StringWork {
+    public static void main(String[] args) {
+        String word = "abcde";
+        System.out.println(findCentralSymbol(word));
+    }
+
+    public static String findCentralSymbol(String word) {
+        char[] chars = word.toCharArray();
+        if (word.length() % 2 == 0) {
+            word = chars[word.length() / 2 - 1] + "" + chars[word.length() / 2];
+        } else {
+            word = chars[word.length() / 2] + "";
+        }
+        return word;
+    }
+
 }
