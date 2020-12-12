5b7a9ab25142aabd5674ae45b4851aa84dbd260d
Alexander <sasha.iovsa@gmail.com>
2020-12-12 19:26:27 +0200

    Create method to reverse String

--- src/lesson5/task2/StringWork.java
+++ src/lesson5/task2/StringWork.java
@@ -2,18 +2,31 @@ package lesson5.task2;
 
 public class StringWork {
     public static void main(String[] args) {
-        String word = "abcde";
+        String word = "abcCde";
         System.out.println(findCentralSymbol(word));
+        System.out.println(reverse(word));
     }
 
     public static String findCentralSymbol(String word) {
-        char[] chars = word.toCharArray();
         if (word.length() % 2 == 0) {
-            word = chars[word.length() / 2 - 1] + "" + chars[word.length() / 2];
+            word = word.substring(word.length()/2-1 , word.length()/2+1);
         } else {
-            word = chars[word.length() / 2] + "";
+            word = word.substring(word.length()/2, word.length()/2 + 1);
         }
         return word;
+    }
+
+    public static String reverse(String str){
+        String first = str.substring(0,str.length()/2);
+        if (str.length()%2 == 0){
+            String second = str.substring(str.length()/2);
+            str = second + first;
+        } else {
+            String second = str.substring(str.length()/2 + 1);
+            String third = str.substring(str.length()/2, str.length()/2 + 1);
+            str = second + third + first;
+        }
+        return str;
     }
 
 }
