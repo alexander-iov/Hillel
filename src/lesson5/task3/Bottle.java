8782b74217e4bffd451f3b8a17f5ccb2742772fb
Alexander <sasha.iovsa@gmail.com>
2020-12-12 19:42:21 +0200

    Init 2 class with some vars

---
+++ src/lesson5/task3/Bottle.java
@@ -0,0 +1,9 @@
+package lesson5.task3;
+
+public class Bottle {
+
+    private int volume;
+    private Liquid liquid;
+    private boolean isCarbonated;
+}
+
---
+++ src/lesson5/task3/Liquid.java
@@ -0,0 +1,7 @@
+package lesson5.task3;
+
+public class Liquid {
+    private int density;
+    private String color;
+
+}
