2642431d9712d63bf26b66d4bfd49c3f19fc3d64
Alexander <sasha.iovsa@gmail.com>
2020-12-20 11:24:07 +0200

    Init commit

---
+++ src/lesson7/task2/IStack.java
@@ -0,0 +1,10 @@
+package lesson7.task2;
+
+public interface IStack {
+
+    void push(int value);
+
+    int pop(); // remove and get value on top of Stack
+
+    int peek(); // get value on top of Stack
+}
---
+++ src/lesson7/task2/StackAndList.java
@@ -0,0 +1,78 @@
+package lesson7.task2;
+
+public class StackAndList implements IStack, IList {
+    @Override
+    public void add(int i) {
+
+    }
+
+    @Override
+    public void insert(int index, int value) {
+
+    }
+
+    @Override
+    public void set(int index, int value) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
+
+    @Override
+    public int getByIndex(int index) {
+        return 0;
+    }
+
+    @Override
+    public Integer getIndexByValue(int value) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return false;
+    }
+
+    @Override
+    public void removeValue(int value) {
+
+    }
+
+    @Override
+    public void removeByIndex(int index) {
+
+    }
+
+    @Override
+    public IList subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public int[] toArray() {
+        return new int[0];
+    }
+
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+}
