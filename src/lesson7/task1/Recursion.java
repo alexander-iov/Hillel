f0e9ca102008b3b0ffcdb187882ccc2f34ee02fa
Alexander <sasha.iovsa@gmail.com>
2020-12-20 11:21:37 +0200

    Make recursive method

--- src/lesson7/task1/Recursion.java
+++ src/lesson7/task1/Recursion.java
@@ -2,6 +2,16 @@ package lesson7.task1;
 
 public class Recursion {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        showArray(array, 0);
+    }
+
+    public static void showArray(int[] array, int position) {
+        if (position >= array.length) {
+            return;
+        } else {
+            System.out.println(array[position]);
+        }
+        showArray(array, position + 1);
     }
 }
