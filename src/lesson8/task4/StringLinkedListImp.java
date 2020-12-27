93ded477f86e9541ad0bbee3da32e5af2aaf969d
alexander-iov <sasha.iovsa@gmail.com>
2020-12-27 18:46:25 +0200

    Create LinkedList class

---
+++ src/lesson8/task4/StringLinkedListImp.java
@@ -0,0 +1,53 @@
+package lesson8.task4;
+
+public class StringLinkedListImp implements StringLinkedList{
+    @Override
+    public void addFirst(String s) {
+
+    }
+
+    @Override
+    public void add(String s, int index) {
+
+    }
+
+    @Override
+    public void addLast(String s) {
+
+    }
+
+    @Override
+    public String getFirst() {
+        return null;
+    }
+
+    @Override
+    public String get(int i) {
+        return null;
+    }
+
+    @Override
+    public String getLast() {
+        return null;
+    }
+
+    @Override
+    public String removeFirst() {
+        return null;
+    }
+
+    @Override
+    public String remove(int i) {
+        return null;
+    }
+
+    @Override
+    public String removeLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
