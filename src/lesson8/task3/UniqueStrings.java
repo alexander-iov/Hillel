61a2d52711c6826c11a17105ac160016aeb5dbdb
alexander-iov <sasha.iovsa@gmail.com>
2020-12-27 17:38:36 +0200

    Create Set

--- src/lesson8/task3/UniqueStrings.java
+++ src/lesson8/task3/UniqueStrings.java
@@ -1,4 +1,15 @@
 package lesson8.task3;
 
+import java.util.*;
+
 public class UniqueStrings {
+    public static void main(String[] args) {
+        String[] strings = {"Pipe", "Book", "Set", "Book", "Window", "List", "Pipe", "Sponge"};
+        List<String> stringList = new ArrayList<>(Arrays.asList(strings));
+        System.out.println(validation(stringList));
+    }
+
+    public static Set<String> validation(List<String> stringList){
+        return new HashSet<>(stringList);
+    }
 }
