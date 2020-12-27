970a3eea321b07a8af74ae57c2354d2d654ff488
alexander-iov <sasha.iovsa@gmail.com>
2020-12-27 18:39:56 +0200

    Complete validation methods

--- src/lesson8/task3/UniqueStrings.java
+++ src/lesson8/task3/UniqueStrings.java
@@ -7,9 +7,20 @@ public class UniqueStrings {
         String[] strings = {"Pipe", "Book", "Set", "Book", "Window", "List", "Pipe", "Sponge"};
         List<String> stringList = new ArrayList<>(Arrays.asList(strings));
         System.out.println(validation(stringList));
+        System.out.println(listValidation(stringList));
     }
 
-    public static Set<String> validation(List<String> stringList){
+    public static Set<String> validation(List<String> stringList) {
         return new HashSet<>(stringList);
+    }
+
+    public static List<String> listValidation(List<String> stringList) {
+        List<String> newList = new ArrayList<>();
+        for (String str : stringList) {
+            if (!(newList.contains(str))) {
+                newList.add(str);
+            }
+        }
+        return newList;
     }
 }
