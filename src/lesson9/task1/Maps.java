af1aff886590ac1d56e1819681b3e8623a5b890a
alexander-iov <sasha.iovsa@gmail.com>
2021-01-01 20:20:06 +0200

    Init commit

---
+++ src/lesson9/task1/Maps.java
@@ -0,0 +1,20 @@
+package lesson9.task1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+    public static void main(String[] args) {
+        Map<String, Integer> maps = new HashMap<>();
+        maps.put("a", 1);
+        maps.put("b", 2);
+        maps.put("c", 3);
+        maps.put("d", 4);
+        maps.put("e", 5);
+        maps.put("f", 6);
+        maps.put("g", 7);
+        maps.put("h", 8);
+        maps.put("j", 9);
+        maps.put("k", 10);
+    }
+}
