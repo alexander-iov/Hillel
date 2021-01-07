86faae4468e00cd2658231849d1f5db363fb7ffa
alexander-iov <sasha.iovsa@gmail.com>
2021-01-07 11:32:12 +0200

    Rename class and update for

--- src/lesson10/task0/ListSum.java
+++ src/lesson10/task0/ListSum.java
@@ -3,7 +3,7 @@ package lesson10.task0;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LIstSum {
+public class ListSum {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
         integers.add(5);
@@ -14,8 +14,8 @@ public class LIstSum {
 
     public static <T extends Number> double sum(List<T> list) {
         double sum = 0;
-        for (int i = 0; i < list.size(); i++) {
-            sum += list.get(i).doubleValue();
+        for (T t : list) {
+            sum += t.doubleValue();
         }
         return sum;
     }
