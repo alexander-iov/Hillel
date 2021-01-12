38f68ecfd6c1e2f559c901468e0124c090b5b6f1
Alexander <sasha.iovsa@gmail.com>
2021-01-12 15:47:10 +0200

    Refactoring Main.java 4

--- src/lesson12/Main.java
+++ src/lesson12/Main.java
@@ -1,27 +1,21 @@
 package lesson12;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
         Report report = new Report();
-        List<Row> rows = new ArrayList<>();
 
         Row row1 = new Row();
         row1.cells[0] = "a";
         row1.cells[1] = "b";
         row1.cells[2] = "c";
-        rows.add(row1);
+        report.rows.add(row1);
 
         Row row2 = new Row();
         row2.cells[0] = "d";
         row2.cells[1] = "e";
         row2.cells[2] = "f";
-        rows.add(row2);
-
-        report.rows = rows;
+        report.rows.add(row2);
 
         for (Row row : report.rows){
             for (String cell : row.cells){
--- src/lesson12/Report.java
+++ src/lesson12/Report.java
@@ -1,9 +1,10 @@
 package lesson12;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Report {
 
-    List<Row> rows;
+    List<Row> rows = new ArrayList<>();
 
 }
