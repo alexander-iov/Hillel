92b40dba2c1453f774d5a555c78b955896b804e3
Alexander <sasha.iovsa@gmail.com>
2021-02-09 18:27:55 +0200

    Final finality complete task2

---
+++ src/lesson18/task2/Main.java
@@ -0,0 +1,24 @@
+package lesson18.task2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Timer;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Customer> queue = new LinkedList<>();
+        Shop shop = new Shop(true, queue);
+        Timer timer = new Timer();
+        timer.schedule(shop, 1_000, 30_000);
+        Shop.AddCustomer addCustomer = new Shop.AddCustomer();
+        Shop.DeleteCustomer deleteCustomer = new Shop.DeleteCustomer();
+        timer.schedule(addCustomer, 1000, 1000 + (random(4) * 1000));
+        timer.schedule(deleteCustomer, 1000, 1000 + (random(7) * 1000));
+    }
+
+    private static int random(int bound) {
+        Random random = new Random();
+        return random.nextInt(bound);
+    }
+}
--- src/lesson18/task2/Shop.java
+++ src/lesson18/task2/Shop.java
@@ -1,25 +1,56 @@
 package lesson18.task2;
 
 import java.util.Queue;
+import java.util.TimerTask;
 
-public class Shop implements Runnable {
+public class Shop extends TimerTask {
     private final int maxCapacity = 5;
-    private boolean isOpen;
-    private final Queue<Customer> queue;
+    private static boolean isOpen;
+    private static Queue<Customer> queue;
 
     public Shop(boolean isOpen, Queue<Customer> queue) {
-        this.isOpen = isOpen;
-        this.queue = queue;
+        Shop.isOpen = isOpen;
+        Shop.queue = queue;
+        System.out.println("Магазин открылся");
     }
 
     private void goToTimeOut() throws InterruptedException {
         isOpen = false;
+        System.out.println("Магазин ушел на перерыв. Клиентов было: " + queue.size());
         Thread.sleep(10_000);
         queue.clear();
     }
 
     @Override
     public void run() {
+        try {
+            goToTimeOut();
+            System.out.println("Магазин снова работает. Клиентов: " + queue.size());
+            isOpen = true;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    static class AddCustomer extends TimerTask {
+
+        @Override
+        public void run() {
+            if (queue.size() < 5 & isOpen) {
+                queue.add(new Customer());
+                System.out.println("Новый покупатель. Клиентов: " + queue.size());
+            }
+        }
+    }
+
+    static class DeleteCustomer extends TimerTask {
+
+        @Override
+        public void run() {
+            if (queue.size() > 1) {
+                queue.remove();
+                System.out.println("Покупатель ушел. Клиентов: " + queue.size());
+            }
+        }
     }
 }
