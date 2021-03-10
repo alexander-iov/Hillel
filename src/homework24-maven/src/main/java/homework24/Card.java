5f0a9b8d196ea9a31926511baa3920558e0d291a
Alexander <sasha.iovsa@gmail.com>
2021-03-10 22:43:40 +0200

    create Card

--- src/homework24-maven/src/main/java/homework24/Card.java
+++ src/homework24-maven/src/main/java/homework24/Card.java
@@ -1,4 +1,23 @@
 package homework24;
 
 public class Card {
+
+    private final String value;
+    private final String suit;
+    private final boolean isTrumpCard;
+
+    public Card(String value, String suit, boolean isTrumpCard) {
+        this.value = value;
+        this.suit = suit;
+        this.isTrumpCard = isTrumpCard;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "value='" + value + '\'' +
+                ", suit='" + suit + '\'' +
+                ", isTrumpCard=" + isTrumpCard +
+                '}';
+    }
 }
