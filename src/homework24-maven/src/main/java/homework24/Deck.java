8c6462e125c6bf3777a9f8d1c377cd61fc7be57f
Alexander <sasha.iovsa@gmail.com>
2021-03-10 22:44:18 +0200

    create Deck

--- src/homework24-maven/src/main/java/homework24/Deck.java
+++ src/homework24-maven/src/main/java/homework24/Deck.java
@@ -1,4 +1,31 @@
 package homework24;
 
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
+
+    private final List<Card> cards;
+
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void mixingDeck(){
+        for (int i = 0; i < cards.size(); i++){
+            Card card = cards.get(random());
+            cards.add(random(), card);
+        }
+    }
+
+    public void print(){
+        for (Card next : cards) {
+            System.out.println(next);
+        }
+    }
+
+    private int random(){
+        Random random = new Random();
+        return random.nextInt(cards.size() - 1);
+    }
 }
